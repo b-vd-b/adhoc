f97777fbe68b21dfd5a38c7035f90837b11dfbb3
Joost <joost_prins@hotmail.com>
2016-04-06 10:02:16 +0200

    begin of KeepAlive

--- src/client/KeepAlive.java
+++ src/client/KeepAlive.java
@@ -1,4 +1,52 @@
 package client;
 
-public class KeepAlive {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.MulticastSocket;
+
+public class KeepAlive implements Runnable {
+
+    private boolean running = true;
+    private MulticastSocket mcSocket;
+    private static final int SLEEP = 1000;
+
+    /**
+     * Constructor with standard socketport 500.
+     */
+    public KeepAlive() {
+        try {
+            mcSocket = new MulticastSocket(500);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Constructor with given port number.
+     * @param port
+     */
+    public KeepAlive(int port) {
+        try {
+            mcSocket = new MulticastSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            try {
+                this.wait(SLEEP);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //mcSocket.send();
+        }
+
+    }
+
+    public void stopKeepAlive() {
+        running = false;
+    }
 }

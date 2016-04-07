0973bd31f55c027de2233b4878c4a6da32d8e98b
Karelkroonen <Karelkroonen@gmail.com>
2016-04-07 11:34:59 +0200

    BroadcastMessage update -Karel

--- src/datatype/BroadcastMessage.java
+++ src/datatype/BroadcastMessage.java
@@ -1,14 +1,22 @@
 package datatype;
 
+import java.net.InetAddress;
+import java.util.HashMap;
+
 public class BroadcastMessage extends Message {
 
     private String nickname;
+    private HashMap<InetAddress,String> destinations;
 
-    public BroadcastMessage(String nickname) {
+    public BroadcastMessage(String nickname, HashMap<InetAddress,String> dest) {
+        destinations = new HashMap<InetAddress,String>();
+        this.destinations = dest;
         this.nickname = nickname;
     }
 
     public String getNickname() {
         return nickname;
     }
+
+    public HashMap<InetAddress,String> getDestinations() { return destinations; }
 }
